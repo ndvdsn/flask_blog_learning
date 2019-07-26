0662151cf1374fe85b7f4d766bc8cb0a72205b33
ndvdsn <neil.j.davidson@gmail.com>
2019-07-26 16:27:38 +0100

    update

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +20,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 from app import routes, models, errors
 
